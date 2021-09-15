477e39456a1cd59385a18b3c5d10a449b1b50c55
Bart Sebela <75604608+Fast-Byte22@users.noreply.github.com>
2021-09-15 19:26:21 +0200

    Add project files.

---
+++ Login/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+//using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+//using var connection = new MySqlConnection();
+
+namespace Login.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("LogIn");
+        }
+
+        //GET
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        //GET
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(string Fn, string LN, string Mobile, string Email, string Pass)
+        {
+            
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
