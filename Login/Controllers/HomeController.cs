4d5c53ee7d412c29fbf0dc6c7dfec8c1346c45d9
Bart Sebela <75604608+Fast-Byte22@users.noreply.github.com>
2021-09-15 19:26:21 +0200

    Add project files.

---
+++ Login/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+//using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+//using var connection = new MySqlConnection();
+
+namespace Login.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+ 
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("LogIn");
+        }
+
+        //GET
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LogIn(LogInViewModel logInViewModelData)
+        {
+
+
+            return View();
+        }
+
+
+        //GET
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(SignUpViewModel SignUpData)
+        {
+            
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Login/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Models
+{
+    public class User
+    {
+        //model info
+        
+        int Id {  get; set; }
+        DateTime CreateTime = DateTime.Now;
+
+        //user info
+        
+        string FirstName {  get; set; }
+        string LastName {  get; set; }
+        string Email {  get; set; }
+        string PhoneNumber {  get; set; }
+        string Password {  get; set; }
+        DateTime DateOfBirth { get; set; }
+    }
+}
